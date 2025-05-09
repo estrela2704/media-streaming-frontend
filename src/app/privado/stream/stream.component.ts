824439448d5b5d29ed27d203c0dca8fe60f62631
Felipe Estrela <felipeestrelatv@gmail.com>
2025-05-09 13:13:59 -0300

    add: angular material

--- src/app/privado/stream/stream.component.ts
+++ src/app/privado/stream/stream.component.ts
@@ -1,10 +1,16 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StreamService } from '../../service/stream.service';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-stream',
-  imports: [],
+  imports: [    
+    MatSlideToggleModule,
+    MatIconModule,
+    ],
   templateUrl: './stream.component.html',
   styleUrl: './stream.component.scss'
 })
@@ -12,13 +18,18 @@ export class StreamComponent implements OnInit {
  
   videoUrl?: string; 
   filename!: string;
-  
+  isDarkMode = false;
+
   private route = inject(ActivatedRoute);
   private streamService = inject(StreamService);
 
   ngOnInit(): void {
     this.filename = this.route.snapshot.paramMap.get("filename")!;
     this.videoUrl = this.streamService.getVideoUrl(this.filename);
+  }
+
+  toggleMode(){
+    this.isDarkMode = !this.isDarkMode;
   }
 
 
