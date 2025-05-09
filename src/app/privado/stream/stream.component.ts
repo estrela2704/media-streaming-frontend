875ce3eb097af2d27dec56a5a3a1ba3e4e07f1f4
Felipe Estrela <felipeestrelatv@gmail.com>
2025-05-09 19:06:06 -0300

    fix: layout e subscriptions

--- src/app/privado/stream/stream.component.ts
+++ src/app/privado/stream/stream.component.ts
@@ -1,11 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StreamService } from '../../service/stream.service';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
-import { map, of, switchMap } from 'rxjs';
+import { map, of, Subscription, switchMap } from 'rxjs';
 import { Mensagem } from '../../interfaces/mensagem.interface';
-
 
 @Component({
   selector: 'app-stream',
@@ -16,47 +15,51 @@ import { Mensagem } from '../../interfaces/mensagem.interface';
   templateUrl: './stream.component.html',
   styleUrl: './stream.component.scss'
 })
-export class StreamComponent implements OnInit {
+export class StreamComponent implements OnInit, OnDestroy {
  
   videoUrl: string | null = null; 
-  videoExists = false;
+  videoExists: boolean | null = null;
   mensagem: Mensagem | null = null;
 
   private route = inject(ActivatedRoute);
   private streamService = inject(StreamService);
 
+  private inscricaoObs?: Subscription;
+
   ngOnInit(): void {
+      this.inscricaoObs = this.route.queryParamMap.pipe(
+        map(params => params.get('v')),
+        switchMap(filename => {
+          if (!filename) {
+            this.videoUrl = null;
+            this.videoExists = false;
+            this.mensagem = {
+              icon: 'info',
+              title: 'Nenhum vídeo selecionado.',
+              detail: 'Adicione um parâmetro ?v=nome-do-arquivo.mp4 na URL para assistir.',
+            };
+            return of(false);
+          }
 
-    this.route.queryParamMap.pipe(
-      map(params => params.get('v')),
-      switchMap(filename => {
-        if(!filename) {
-          this.videoUrl = null;
-          this.videoExists = false;
-          this.mensagem = {
-            icon: 'info',
-            title: 'Nenhum vídeo selecionado.',
-            detail: 'Adicione um parâmetro ?v=nome-do-arquivo.mp4 na URL para assistir.',
-          };
-          return of(false)
-        };
-
-        this.videoUrl = this.streamService.getVideoUrl(filename);
-        this.mensagem = null;
-        return this.streamService.videoExists(filename);
-      })
+          this.videoUrl = this.streamService.getVideoUrl(filename);
+          this.mensagem = null;
+          return this.streamService.videoExists(filename);
+        }),
       ).subscribe((exists: boolean) => {
         this.videoExists = exists;
-
-        if(!exists && this.videoUrl){
-            this.mensagem = {
-              icon: 'videocam_off',
-              title: 'Oops! Este vídeo não está disponível no momento.',
-              detail: 'Verifique se o arquivo foi enviado corretamente ou tente novamente mais tarde.',
-            };
+        if (!exists && this.videoUrl) {
+          this.mensagem = {
+            icon: 'videocam_off',
+            title: 'Oops! Este vídeo não está disponível no momento.',
+            detail: 'Verifique se o arquivo foi enviado corretamente ou tente novamente mais tarde.',
+          };
         }
-
       })
+    
+  }
+
+  ngOnDestroy(): void {
+    this.inscricaoObs?.unsubscribe();
   }
 
 }
